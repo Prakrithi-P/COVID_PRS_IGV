1812b919ec77aac582fe461035a43b7140876089
Prakrithi-P <70966155+Prakrithi-P@users.noreply.github.com>
2021-03-30 12:12:51 +0530

    Update script.R

--- script.R
+++ script.R
@@ -27,41 +27,68 @@ par(mar=c(5,15,5,1))
 plot(thsd)
 
 ## District level COVID19 information for corresponding IGV pops collected from https://www.covid19india.org/ and https://covid19.assam.gov.in/district/
-#PRS-Mortality - Spearman correlation
+#PRS-Mortality - Pearson's correlation
 c<-read.csv("prs_deaths", sep="\t",header=T)  ##Fields in prs_deaths - POP,PRS,District,No.of Deaths
-ggscatter(c,x="PRS",y="Deaths",add="reg.line",conf.int=T, cor.coef=T, cor.method = "spearman",ylab = "No.of deaths due to COVID",title = "Spearman correlation")  ##correlation plot
+ggscatter(c,x="PRS",y="Deaths",add="reg.line",conf.int=T, cor.coef=T, cor.method = "pearson",ylab = "No.of deaths due to COVID")  ##correlation plot
 
        
 ########################################################
 ## VarLD - Comparison of LD around SNPs
 ##Genotype file 12 format
-system(paste(“/opt/apps/plink-1.07-x86_64/plink --bfile CEU_1000G_all --chr 12 --from-bp 108383145 --to-bp 118375718 --extract 1000g_snps_list  --recode12 --transpose --tab --noweb --out CEU_rs10735079_10mb”, sep=” “)
-system(paste(“/opt/apps/plink-1.07-x86_64/plink --bfile ITU_1000G_all --chr 12 --from-bp 108383145 --to-bp 118375718 --extract 1000g_snps_list  --recode12 --transpose --tab --noweb --out ITU_rs10735079_10mb”, sep=” “)
-system(paste(“/opt/apps/plink-1.07-x86_64/plink --bfile PJL_1000G_all --chr 12 --from-bp 108383145 --to-bp 118375718 --extract 1000g_snps_list  --recode12 --transpose --tab --noweb --out PJL_rs10735079_10mb”, sep=” “)
+system(paste(“/opt/apps/plink-1.07-x86_64/plink --bfile CEU_1000G_all  --extract range top100_snps_10MB_list  --recode12 --transpose --tab --noweb --out CEU_100snps_10mb”, sep=” “)
+system(paste(“/opt/apps/plink-1.07-x86_64/plink --bfile CEU_1000G_all  --extract range top100_snps_10MB_list  --recode12 --transpose --tab --noweb --out CEU_100snps_10mb”, sep=” “)
 
 ##Input SNP pos genotype1 genotype2 …
-##Convert Genotype file to 1234 format
+##Convert Genotype file to 1234 format -- for each chromosome separately
 system(paste(“sed  -r 's/(\s+)?\S+//3' CEU_rs10735079_10mb.tped | sed  -r 's/(\s+)?\S+//1' | sed 's/2 2/1/g'  | sed 's/1 1/3/g' | sed 's/2 1/2/g' | sed 's/1 2/2/g' | sed 's/2 0/4/g' | sed 's/0 2/4/g' | sed 's/0 1/4/g' | sed 's/0 0/4/g' | sed 's/\trs/rs/g' > CEU_rs10735079_infile”, sep=” “)
 system(paste(“sed  -r 's/(\s+)?\S+//3' ITU_rs10735079_10mb.tped | sed  -r 's/(\s+)?\S+//1' | sed 's/2 2/1/g'  | sed 's/1 1/3/g' | sed 's/2 1/2/g' | sed 's/1 2/2/g' | sed 's/2 0/4/g' | sed 's/0 2/4/g' | sed 's/0 1/4/g' | sed 's/0 0/4/g' | sed 's/\trs/rs/g' > ITU_rs10735079_infile”, sep=” “)
 system(paste(“sed  -r 's/(\s+)?\S+//3' PJL_rs10735079_10mb.tped | sed  -r 's/(\s+)?\S+//1' | sed 's/2 2/1/g'  | sed 's/1 1/3/g' | sed 's/2 1/2/g' | sed 's/1 2/2/g' | sed 's/2 0/4/g' | sed 's/0 2/4/g' | sed 's/0 1/4/g' | sed 's/0 0/4/g' | sed 's/\trs/rs/g' > PJL_rs10735079_infile”, sep=” “)
 
 ##VarLD: 
-system(paste(“java -jar rgenetics-1.0.jar -p VarLD CEU_rs10735079_infile ITU_rs10735079_infile -o CEU_ITU_rs10735079",sep=" ")
-system(paste(“java -jar rgenetics-1.0.jar -p VarLD PJL_rs10735079_infile ITU_rs10735079_infile -o PJL_ITU_rs10735079",sep=" ")
+system(paste(“for i in {1..22} ; do java -jar rgenetics-1.0.jar -p VarLD CEU_chr$i\_infile ITU_chr$i\_infile -o CEU_ITU_chr$i ; done",sep=" ")
+#system(paste(“for i in {1..22} ; do java -jar rgenetics-1.0.jar -p VarLD CHB_chr$i\_infile ITU_chr$i\_infile -o CEU_ITU_chr$i ; done",sep=" ")
 
-##Plot r ---> To plot a region
-dd<-read.csv("VARLD_ALL", sep="\t", header=T)   ## concatenate results of CEU_ITU_rs10735079 and PJL_ITU_rs10735079 and add population labels
-dd$PhysicalPosition <- dd$position/1000000
-ggplot(dd,aes(PhysicalPosition,standardized_score))+geom_point(aes(PhysicalPosition,standardized_score,color= Population),alpha=0.5)+xlab("Physical Position (Mb)")+theme_minimal()+scale_colour_manual(values=c("red","blue","green","black","orange","violet"))
+##standardize the varLD output with the R script provided by the VarLD package as below     
+Standardization
+varLD.out <- {}
+chr.store <- {}
+for (chr in 1:22){
+    varLD.temp <- read.table(paste("CEU_ITU_chr", chr, sep=""), sep="\t", header = T)
+varLD.out <- rbind(varLD.out, varLD.temp)
+chr.store <- c(chr.store, rep(chr, dim(varLD.temp)[1]))
+print(paste("completed reading in unstandardized varLD output file for chromosome ", chr, sep=""))
+}
+varLD.mean <- mean(varLD.out[,"raw_score"])
+varLD.sd <- sd(varLD.out[,"raw_score"])
+standardized_score <- (varLD.out[,"raw_score"] - varLD.mean)/varLD.sd
+varLD.out <- cbind(varLD.out, standardized_score)
+varLD.threshold <- quantile(standardized_score, probs = percentile.out)
+for (chr in 1:22){
+    chr.flag <- which(chr.store == chr)
+    write.table(varLD.out[chr.flag,], paste("CEU_ITU_chr", chr, "_standardized.out", sep=""), sep="\t", quote=F, row.names=F)
+    print(paste("completed writing out standardized varLD output file for chromosome ", chr, sep=""))
+}
+n.length.percentile <- length(percentile.out)
+for (i in 1:n.length.percentile){
+    print(paste("varLD threshold for ", percentile.out[i], " = ", varLD.threshold[i], sep=""))
+} 
+
+##varLD thresholds
+[1] "varLD threshold for 0.95 = 1.78136769083323"
+[1] "varLD threshold for 0.99 = 3.37859820232148"
+[1] "varLD threshold for 0.999 = 6.87171244835101"
+[1] "varLD threshold for 0.9999 = 12.4041619384797"
 
 ## To show variants and their LD patterns across multiple chromosomes
 library(qqman)
-d<-read.csv("varld_maf1_all", header=TRUE, sep="\t")
-h<-read.csv("highlight_100", sep="\t", header=T)
+##add chromosome labels and concatenate the chr-wise standardized output files and use as follows:       
+d<-read.csv("varld_standardized.out", header=TRUE, sep="\t")
+h<-read.csv("highlight_100", sep="\t", header=T). ## list of positions to highlight
 hs<-as.character(h$pos)
 manhattan(d, chr = "Chr", bp = "pos", p = "Standardized_score", snp = "pos",
 col = c("gray10", "gray60"), chrlabs = NULL,suggestiveline = 3.37, genomewideline = 6.87,highlight = hs, logp = FALSE,ylim=c(-2,20), ylab=”Standardized VarLD score”)
-
+##suggestiveline, genomewideline values are based on the varld standardization script
+       
 #########################################################
 ##Spatial Plot-- Modified verion of the already avalable code based on IDW algorithm
 library(rgdal)
@@ -93,9 +120,6 @@ tm_shape(shp) + tm_polygons() +
           title="PRS", size=0.7) +
   tm_text("PRS", just="left", xmod=.5, size = 0.7) +
   tm_legend(legend.outside=TRUE)
-####op warning: Warning messages:
-#1: The argument auto.palette.mapping is deprecated. Please use midpoint for numeric data and stretch.palette for categorical data to control the palette mapping. 
-#2: Currect projection of shape shp unknown. Long-lat (WGS84) is assumed. 
 
 th  <-  as(dirichlet(as.ppp(p)), "SpatialPolygons")
 
@@ -111,7 +135,7 @@ proj4string(th) <- proj4string(p)
 th.z     <- over(th, p, fn=mean)
 th.spdf  <-  SpatialPolygonsDataFrame(th, th.z)
 
-# Finally, we'll clip the tessellated  surface to the Texas boundaries
+# Finally, we'll clip the tessellated  surface to the India boundaries
 set_RGEOS_CheckValidity(2L)
 th.clp   <- intersect(shp,th.spdf)
 
@@ -142,5 +166,5 @@ r.m     <- mask(r, shp)
 tm_shape(r.m) + 
   tm_raster(n=10,palette = "Reds", auto.palette.mapping = FALSE,
             title="PRS") + 
-  tm_shape(p) + tm_dots(size=0.2) +
+  tm_shape(p) + tm_dots(size=0.02) +
   tm_legend(legend.outside=TRUE)+tm_legend(legend.outside=TRUE)+tm_text("POP", just="top", xmod=0.7, size = 0.6)
